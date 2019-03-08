75dcbcd005685eaeee5a131c90c274fdd8af96e1
Brandon Schabell <brandonschabell@gmail.com>
2019-03-07 20:48:36 -0600

    added module for in-memory video splitting

---
+++ videotoframes/__init__.py
@@ -0,0 +1,38 @@
+import base64
+import cv2
+from tempfile import NamedTemporaryFile
+
+from videotoframes.video_to_frames import get_frames_to_grab
+
+
+def convert(video_base_64, max_frames=None, even=False):
+	with NamedTemporaryFile('wb') as video_file:
+		video_file.write(base64.b64decode(video_base_64))
+		video = cv2.VideoCapture(video_file.name)
+
+		count = 0
+
+		frames = []
+
+		frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+		if max_frames is None:
+			max_frames = frame_count
+
+		if even:
+			grab_frames = get_frames_to_grab(frame_count=frame_count, max_frames=max_frames)
+
+			for frame_num in grab_frames:
+				video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+				ret, frame = video.read()
+				ret, buffer = cv2.imencode('.jpg', frame)
+				frames.append(base64.b64encode(buffer))
+		else:
+			while count < max_frames:
+				ret, frame = video.read()
+				if frame is not None:
+					count += 1
+					ret, buffer = cv2.imencode('.jpg', frame)
+					frames.append(base64.b64encode(buffer))
+
+		video.release()
+		return frames
