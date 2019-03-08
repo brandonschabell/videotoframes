4cac7712c79313a77110d7e2e19a16d2341b6994
Brandon Schabell <brandonschabell@gmail.com>
2019-03-07 21:20:42 -0600

    added ability to set frame rate for video conversion

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -27,8 +27,50 @@ def test_main_max_frames_even():
 	assert len(frames) == 10
 
 
-def test_convert_max_frames_even_2_frames(tmpdir):
+def test_convert_max_frames_even_2_frames():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, max_frames=2, even=True)
 	assert len(frames) == 2
+
+
+def test_convert_frame_rate():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, frame_rate=2)
+	assert len(frames) == 12
+
+
+def test_convert_frame_rate_divisible():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, frame_rate=15)
+	assert len(frames) == 83
+
+
+def test_convert_frame_rate_full():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, frame_rate=30)
+	assert len(frames) == 166
+
+
+def test_convert_frame_rate_higher_than_full():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, frame_rate=40)
+	assert len(frames) == 166
+
+
+def test_convert_frame_rate_max_frames():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, max_frames=8, frame_rate=2)
+	assert len(frames) == 8
+
+
+def test_convert_frame_rate_max_frames_higher_than_frame_count():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, max_frames=13, frame_rate=2)
+	assert len(frames) == 12
--- videotoframes/__init__.py
+++ videotoframes/__init__.py
@@ -5,7 +5,7 @@ from tempfile import NamedTemporaryFile
 from videotoframes.video_to_frames import get_frames_to_grab
 
 
-def convert(video_base_64, max_frames=None, even=False):
+def convert(video_base_64, frame_rate=None, max_frames=None, even=False):
 	with NamedTemporaryFile('wb') as video_file:
 		video_file.write(base64.b64decode(video_base_64))
 		video = cv2.VideoCapture(video_file.name)
@@ -26,6 +26,21 @@ def convert(video_base_64, max_frames=None, even=False):
 				ret, frame = video.read()
 				ret, buffer = cv2.imencode('.jpg', frame)
 				frames.append(base64.b64encode(buffer))
+		elif frame_rate is not None:
+			fps = video.get(cv2.CAP_PROP_FPS)
+			if frame_rate > fps:
+				frame_rate = fps
+			time_stamp = 0
+			while count < max_frames:
+				video.set(cv2.CAP_PROP_POS_MSEC, round(time_stamp))
+				ret, frame = video.read()
+				if frame is not None:
+					count += 1
+					ret, buffer = cv2.imencode('.jpg', frame)
+					frames.append(base64.b64encode(buffer))
+				else:
+					break
+				time_stamp += 1000 / frame_rate
 		else:
 			while count < max_frames:
 				ret, frame = video.read()
