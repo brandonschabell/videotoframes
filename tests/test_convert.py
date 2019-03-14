36e655cdbe0a081d8cee152c41deb09197df7e4a
Brandon Schabell <brandonschabell@gmail.com>
2019-03-13 20:23:21 -0500

    added max timestamp to convert

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -10,6 +10,7 @@ def test_convert():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 166
 
 
@@ -17,6 +18,7 @@ def test_convert_max_frames():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, max_frames=10)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 10
 
 
@@ -24,6 +26,7 @@ def test_main_max_frames_even():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, max_frames=10, even=True)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 10
 
 
@@ -31,6 +34,7 @@ def test_convert_max_frames_even_2_frames():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, max_frames=2, even=True)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 2
 
 
@@ -38,13 +42,23 @@ def test_convert_frame_rate():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, frame_rate=2)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 12
+
+
+def test_convert_frame_rate_small():
+	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
+		video_base_64 = base64.b64encode(file.read()).decode()
+	frames = convert(video_base_64, frame_rate=1)
+	assert frames[0] != frames[-1]
+	assert len(frames) == 6
 
 
 def test_convert_frame_rate_divisible():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, frame_rate=15)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 83
 
 
@@ -52,6 +66,7 @@ def test_convert_frame_rate_full():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, frame_rate=30)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 166
 
 
@@ -59,6 +74,7 @@ def test_convert_frame_rate_higher_than_full():
 	with open(os.path.join(get_testfiles_path(), 'small.mp4'), 'rb') as file:
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, frame_rate=40)
+	assert frames[0] != frames[-1]
 	assert len(frames) == 166
 
 
@@ -67,6 +83,7 @@ def test_convert_frame_rate_max_frames():
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, max_frames=8, frame_rate=2)
 	assert len(frames) == 8
+	assert frames[0] != frames[-1]
 
 
 def test_convert_frame_rate_max_frames_higher_than_frame_count():
@@ -74,3 +91,4 @@ def test_convert_frame_rate_max_frames_higher_than_frame_count():
 		video_base_64 = base64.b64encode(file.read()).decode()
 	frames = convert(video_base_64, max_frames=13, frame_rate=2)
 	assert len(frames) == 12
+	assert frames[0] != frames[-1]
