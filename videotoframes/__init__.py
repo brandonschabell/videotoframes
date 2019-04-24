67b6d81b99991cee1a311ba8378a84dcdc1182fd
Brandon Schabell <brandonschabell@gmail.com>
2019-04-23 19:26:37 -0500

    replaced tabs with spaces

--- videotoframes/__init__.py
+++ videotoframes/__init__.py
@@ -10,98 +10,98 @@ DATE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
 def convert(video_base_64, frame_rate=None, max_frames=None, even=False, video_timestamp=None, return_dict=False):
-	if video_timestamp is not None and not return_dict:
-		warnings.warn('Ignoring return_dict=False since a video_timestamp has been specified.')
-		return_dict = True
-	if not return_dict:
-		warnings.warn(
-			'Returning a list instead of a list of dictionaries. This option will be removed in a future release when all frames will be returned in dictionaries.',
-			DeprecationWarning)
+    if video_timestamp is not None and not return_dict:
+        warnings.warn('Ignoring return_dict=False since a video_timestamp has been specified.')
+        return_dict = True
+    if not return_dict:
+        warnings.warn(
+            'Returning a list instead of a list of dictionaries. This option will be removed in a future release when all frames will be returned in dictionaries.',
+            DeprecationWarning)
 
-	if video_timestamp is not None:
-		video_datetime = datetime.strptime(video_timestamp, DATE_TIME_FORMAT)
+    if video_timestamp is not None:
+        video_datetime = datetime.strptime(video_timestamp, DATE_TIME_FORMAT)
 
-	with NamedTemporaryFile('wb') as video_file:
-		video_file.write(base64.b64decode(video_base_64))
-		video = cv2.VideoCapture(video_file.name)
+    with NamedTemporaryFile('wb') as video_file:
+        video_file.write(base64.b64decode(video_base_64))
+        video = cv2.VideoCapture(video_file.name)
 
-		count = 0
+        count = 0
 
-		frames = []
+        frames = []
 
-		frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-		video.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 1)
-		_, frame = video.read()
-		if frame is None:
-			frame_count -= 1
-		video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 1)
+        _, frame = video.read()
+        if frame is None:
+            frame_count -= 1
+        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-		if max_frames is None:
-			max_frames = frame_count
+        if max_frames is None:
+            max_frames = frame_count
 
-		if even:
-			grab_frames = get_frames_to_grab(frame_count=frame_count, max_frames=max_frames)
+        if even:
+            grab_frames = get_frames_to_grab(frame_count=frame_count, max_frames=max_frames)
 
-			for frame_num in grab_frames:
-				video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-				ret, frame = video.read()
-				if frame is not None:
-					ret, buffer = cv2.imencode('.jpg', frame)
-					return_frame = base64.b64encode(buffer)
-					if return_dict:
-						return_frame = {
-							'base64image': return_frame
-						}
-						if video_timestamp != None:
-							frame_offset_ms = video.get(cv2.CAP_PROP_POS_MSEC)
-							frame_datetime = video_datetime + timedelta(milliseconds=frame_offset_ms)
-							return_frame['timestamp'] = frame_datetime.strftime(DATE_TIME_FORMAT)
-					frames.append(return_frame)
-				else:
-					break
-		elif frame_rate is not None:
-			fps = video.get(cv2.CAP_PROP_FPS)
-			if frame_rate > fps:
-				frame_rate = fps
-			time_stamp = 0
-			max_timestamp = frame_count * 1000 / fps
-			while count < max_frames and time_stamp <= max_timestamp:
-				video.set(cv2.CAP_PROP_POS_MSEC, round(time_stamp))
-				ret, frame = video.read()
-				if frame is not None:
-					count += 1
-					ret, buffer = cv2.imencode('.jpg', frame)
-					return_frame = base64.b64encode(buffer)
-					if return_dict:
-						return_frame = {
-							'base64image': return_frame
-						}
-						if video_timestamp != None:
-							frame_offset_ms = video.get(cv2.CAP_PROP_POS_MSEC)
-							frame_datetime = video_datetime + timedelta(milliseconds=frame_offset_ms)
-							return_frame['timestamp'] = frame_datetime.strftime(DATE_TIME_FORMAT)
-					frames.append(return_frame)
-				else:
-					break
-				time_stamp += 1000 / frame_rate
-		else:
-			while count < max_frames:
-				ret, frame = video.read()
-				if frame is not None:
-					count += 1
-					ret, buffer = cv2.imencode('.jpg', frame)
-					return_frame = base64.b64encode(buffer)
-					if return_dict:
-						return_frame = {
-							'base64image': return_frame
-						}
-						if video_timestamp != None:
-							frame_offset_ms = video.get(cv2.CAP_PROP_POS_MSEC)
-							frame_datetime = video_datetime + timedelta(milliseconds=frame_offset_ms)
-							return_frame['timestamp'] = frame_datetime.strftime(DATE_TIME_FORMAT)
-					frames.append(return_frame)
-				else:
-					break
+            for frame_num in grab_frames:
+                video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+                ret, frame = video.read()
+                if frame is not None:
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    return_frame = base64.b64encode(buffer)
+                    if return_dict:
+                        return_frame = {
+                            'base64image': return_frame
+                        }
+                        if video_timestamp != None:
+                            frame_offset_ms = video.get(cv2.CAP_PROP_POS_MSEC)
+                            frame_datetime = video_datetime + timedelta(milliseconds=frame_offset_ms)
+                            return_frame['timestamp'] = frame_datetime.strftime(DATE_TIME_FORMAT)
+                    frames.append(return_frame)
+                else:
+                    break
+        elif frame_rate is not None:
+            fps = video.get(cv2.CAP_PROP_FPS)
+            if frame_rate > fps:
+                frame_rate = fps
+            time_stamp = 0
+            max_timestamp = frame_count * 1000 / fps
+            while count < max_frames and time_stamp <= max_timestamp:
+                video.set(cv2.CAP_PROP_POS_MSEC, round(time_stamp))
+                ret, frame = video.read()
+                if frame is not None:
+                    count += 1
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    return_frame = base64.b64encode(buffer)
+                    if return_dict:
+                        return_frame = {
+                            'base64image': return_frame
+                        }
+                        if video_timestamp != None:
+                            frame_offset_ms = video.get(cv2.CAP_PROP_POS_MSEC)
+                            frame_datetime = video_datetime + timedelta(milliseconds=frame_offset_ms)
+                            return_frame['timestamp'] = frame_datetime.strftime(DATE_TIME_FORMAT)
+                    frames.append(return_frame)
+                else:
+                    break
+                time_stamp += 1000 / frame_rate
+        else:
+            while count < max_frames:
+                ret, frame = video.read()
+                if frame is not None:
+                    count += 1
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    return_frame = base64.b64encode(buffer)
+                    if return_dict:
+                        return_frame = {
+                            'base64image': return_frame
+                        }
+                        if video_timestamp != None:
+                            frame_offset_ms = video.get(cv2.CAP_PROP_POS_MSEC)
+                            frame_datetime = video_datetime + timedelta(milliseconds=frame_offset_ms)
+                            return_frame['timestamp'] = frame_datetime.strftime(DATE_TIME_FORMAT)
+                    frames.append(return_frame)
+                else:
+                    break
 
-		video.release()
-		return frames
+        video.release()
+        return frames
--- videotoframes/video_to_frames.py
+++ videotoframes/video_to_frames.py
@@ -5,77 +5,77 @@ from tqdm import tqdm
 
 
 def get_frames_to_grab(frame_count, max_frames):
-	span = (frame_count -1) / (max_frames - 1)
-	return [round(i * span) for i in range(max_frames)]
+    span = (frame_count -1) / (max_frames - 1)
+    return [round(i * span) for i in range(max_frames)]
 
 
 def video_to_frames(input_path, output_dir, max_frames, even):
-	video = cv2.VideoCapture(input_path)
+    video = cv2.VideoCapture(input_path)
 
-	count = 0
+    count = 0
 
-	base_filename = input_path.split(os.path.sep)[-1].split('.')[0]
+    base_filename = input_path.split(os.path.sep)[-1].split('.')[0]
 
-	frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-	video.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 1)
-	ret, frame = video.read()
-	if frame is None:
-		frame_count -= 1
-	video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    video.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 1)
+    ret, frame = video.read()
+    if frame is None:
+        frame_count -= 1
+    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-	if max_frames is None:
-		max_frames = frame_count
+    if max_frames is None:
+        max_frames = frame_count
 
-	if even:
-		grab_frames = get_frames_to_grab(frame_count=frame_count, max_frames=max_frames)
+    if even:
+        grab_frames = get_frames_to_grab(frame_count=frame_count, max_frames=max_frames)
 
-		for frame_num in grab_frames:
-			video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-			ret, frame = video.read()
-			if frame is not None:
-				output_path = os.path.join(output_dir, base_filename + '-frame{:03d}.jpg'.format(frame_num))
-				cv2.imwrite(output_path, frame)
-			else:
-				break
-	else:
-		while count < max_frames:
-			ret, frame = video.read()
-			if frame is not None:
-				output_path = os.path.join(output_dir, base_filename + '-frame{:03d}.jpg'.format(count))
-				count += 1
-				cv2.imwrite(output_path, frame)
-			else:
-				break
+        for frame_num in grab_frames:
+            video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+            ret, frame = video.read()
+            if frame is not None:
+                output_path = os.path.join(output_dir, base_filename + '-frame{:03d}.jpg'.format(frame_num))
+                cv2.imwrite(output_path, frame)
+            else:
+                break
+    else:
+        while count < max_frames:
+            ret, frame = video.read()
+            if frame is not None:
+                output_path = os.path.join(output_dir, base_filename + '-frame{:03d}.jpg'.format(count))
+                count += 1
+                cv2.imwrite(output_path, frame)
+            else:
+                break
 
-	video.release()
-	return True
+    video.release()
+    return True
 
 
 def main(args=None):
-	parser = argparse.ArgumentParser(prog='videotoframes')
-	parser.add_argument('-o', required=True, help='output directory')
-	parser.add_argument('-i', required=True, nargs='+', help='input video(s).')
-	parser.add_argument('--max-frames', type=int, help='optionally include a maximum number of frames into which the '
+    parser = argparse.ArgumentParser(prog='videotoframes')
+    parser.add_argument('-o', required=True, help='output directory')
+    parser.add_argument('-i', required=True, nargs='+', help='input video(s).')
+    parser.add_argument('--max-frames', type=int, help='optionally include a maximum number of frames into which the '
                                                        'video will be broken.')
-	parser.add_argument('--even', action='store_true', help='If a "--max-frames" value is used, this will sample '
+    parser.add_argument('--even', action='store_true', help='If a "--max-frames" value is used, this will sample '
                                                             'frames from a uniform distribution as opposed to using '
                                                             'sequential frames.')
-	args = parser.parse_args(args)
-	output_dir = args.o
+    args = parser.parse_args(args)
+    output_dir = args.o
 
-	if not os.path.isdir(output_dir):
-		os.mkdir(output_dir)
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
-	inp = args.i
-	if os.path.isdir(inp[0]):
-		inp = [os.path.join(inp[0], f) for f in os.listdir(inp[0]) if os.path.isfile(os.path.join(inp[0], f))]
+    inp = args.i
+    if os.path.isdir(inp[0]):
+        inp = [os.path.join(inp[0], f) for f in os.listdir(inp[0]) if os.path.isfile(os.path.join(inp[0], f))]
 
-	if len(inp) == 0:
-		raise Exception('No video selected.')
+    if len(inp) == 0:
+        raise Exception('No video selected.')
 
-	max_frames = args.max_frames
-	even = args.even
+    max_frames = args.max_frames
+    even = args.even
 
-	for filename in tqdm(inp, desc='Breaking videos into frames'):
-		video_to_frames(filename, output_dir, max_frames, even)
+    for filename in tqdm(inp, desc='Breaking videos into frames'):
+        video_to_frames(filename, output_dir, max_frames, even)
