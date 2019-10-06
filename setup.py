9da22b8a4269bdfa058658f377752b269a7a9e88
Brandon Schabell <brandonschabell@gmail.com>
2019-10-06 13:41:47 -0500

    incremented version to 1.1.5

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ test_requirements = [
 
 setup(
     name='videotoframes',
-    version='1.1.4',
+    version='1.1.5',
     packages=find_packages(),
     url='https://github.com/brandonschabell/videotoframes',
-    download_url='https://github.com/brandonschabell/videotoframes/archive/v1.1.4.tar.gz',
+    download_url='https://github.com/brandonschabell/videotoframes/archive/v1.1.5.tar.gz',
     license='MIT',
     author='Brandon Schabell',
     author_email='brandonschabell@gmail.com',
